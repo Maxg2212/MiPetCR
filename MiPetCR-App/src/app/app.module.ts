835e7b4365fe4af3077002220680ad97ad923149
Naheem Johnson <naheemjohnson@estudianTEC.cr>
2024-05-29 19:47:22 -0600

    Home creado

--- MiPetCR-App/src/app/app.module.ts
+++ MiPetCR-App/src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './view/home/home.component';
+import { FooterComponent } from './view/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      { 
+        path: '', 
+        component: HomeComponent 
+      }
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
